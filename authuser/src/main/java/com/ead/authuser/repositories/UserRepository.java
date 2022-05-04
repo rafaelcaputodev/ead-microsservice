29e31cda2ae58e82f2fd4fe7d4d501fbe9b157d6
rafaelcaputodev <rafaelcaputodev@gmail.com>
2022-05-03 23:41:23 -0300

    implements api user (GetAll, GetById, Delete)

--- authuser/src/main/java/com/ead/authuser/repositories/UserRepository.java
+++ authuser/src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<UUID, UserModel> {
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
 }
